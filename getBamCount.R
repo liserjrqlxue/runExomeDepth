10a7e24a5f58c956adaa634b1a8402ed2a3a29bd
liser.jrqlxue <liser.jrqlxue@gmail.com>
2018-11-14 09:12:08 +0800

    add getBamCount for single bam

---
+++ getBamCount.R
@@ -0,0 +1,32 @@
+getBamCount <- function(
+    target,
+    bam, 
+    index = bam,
+    min.mapq = 20, 
+    read.width = 300, 
+    prefix = 'exons.hg19'
+    ) {
+    
+    my.param <- Rsamtools::ScanBamParam(
+        flag = Rsamtools::scanBamFlag(
+            isDuplicate = FALSE, 
+            isPaired = TRUE, 
+            isProperPair = TRUE, 
+            isSecondaryAlignment = FALSE
+            ),
+        what = c("mapq", "pos", "isize"),
+        )
+    target =readRDS(paste0(prefix,".target.rds"))
+    
+    bam <- bam.file
+    index <- index.file
+    t <- countBamInGRanges.exomeDepth(
+        bam.file = bam, 
+        index = index, 
+        granges = target, 
+        min.mapq = min.mapq, 
+        read.width = read.width
+        )
+    message("Number of counted fragments : ", sum(t))
+    return t
+}
