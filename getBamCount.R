6a3436b1d62d6eaf01f25677902e1422c3b38848
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2018-12-11 10:38:44 +0800

    clean unsed param

--- getBamCount.R
+++ getBamCount.R
@@ -3,20 +3,8 @@ getBamCount <- function(
     bam, 
     index = bam,
     min.mapq = 20, 
-    read.width = 300, 
-    prefix = 'exons.hg19'
+    read.width = 300
     ) {
-    
-    my.param <- Rsamtools::ScanBamParam(
-        flag = Rsamtools::scanBamFlag(
-            isDuplicate = FALSE, 
-            isPaired = TRUE, 
-            isProperPair = TRUE, 
-            isSecondaryAlignment = FALSE
-            ),
-        what = c("mapq", "pos", "isize"),
-        )
-    target =readRDS(paste0(prefix,".target.rds"))
     
     t <- countBamInGRanges.exomeDepth(
         bam.file = bam, 
