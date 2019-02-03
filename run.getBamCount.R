b6cf772a2b0b90f3434c48e2ee4b631fdb080fd8
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-02-03 10:34:00 +0800

    add sourceDir

--- run.getBamCount.R
+++ run.getBamCount.R
@@ -4,8 +4,14 @@ sampleID=args[1]
 bam=args[2]
 outdir=args[3]
 library(ExomeDepth)
-source("getBamCount.R")
-target <- readRDS("exons.hg19.target.rds")
+if(length(args)>3){
+	sourceDir=args[4]
+	source(paste(sourceDir,"getBamCount.R",sep="/"))
+	target <- readRDS(paste(sourceDir,"exons.hg19.target.rds",sep="/"))
+}else{
+	source("getBamCount.R")
+	target <- readRDS("exons.hg19.target.rds")
+}
 my.count <- getBamCount(
 	target = target,
 	bam = bam
