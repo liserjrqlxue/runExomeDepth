7c9dd8887e677875a0c3b7f16be1af72b5858b9a
Wang Yaoshen <wangyaoshen@bgi.com>
2021-09-15 15:40:04 +0800

    add run.getBamCount.sh

---
+++ run.getBamCount.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+set -euo pipefail
+Bin=$(dirname $(readlink -f "$0"))
+sampleID=$1
+bam=$2
+gender=$3
+outDir=$4
+
+# input  : $bam
+# output : $outDir/$sampleID.A.my.count.rds $outDir/$sampleID.$gender.my.count.rds
+
+mkdir -p $outDir
+
+Rscript $Bin/run.getBamCount.R $sampleID $bam  A       $outDir $Bin 
+
+Rscript $Bin/run.getBamCount.R $sampleID $bam  $gender $outDir $Bin 
